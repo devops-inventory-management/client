7b7d67887c2b2e59040a4c5ce81900439847784e
delhombre <brunodogbase@gmail.com>
2024-08-07 12:23:18 +0200

    📝 (navbar.tsx): comment out BellIcon and UserIcon components as they are not being used in the navbar   💡 (navbar.tsx): add comments to clarify commented out code segments and improve code readability

--- src/app/(components)/navbar.tsx
+++ src/app/(components)/navbar.tsx
@@ -2,13 +2,11 @@
 
 import { setIsDarkMode, setIsSidebarCollapsed } from "@/state";
 import {
-	BellIcon,
 	MenuIcon,
 	MoonIcon,
 	SearchIcon,
 	SettingsIcon,
 	SunIcon,
-	UserIcon,
 } from "lucide-react";
 import Link from "next/link";
 import { useAppDispatch, useAppSelector } from "../redux";
@@ -64,17 +62,17 @@ export default function Navbar() {
 							)}
 						</button>
 					</div>
-					<div className="relative">
+					{/* <div className="relative">
 						<BellIcon className="cursor-pointer text-gray-500" size={24} />
 						<span className="absolute -top-2 -right-2 inline-flex items-center justify-center px-[0.4rem] py-1 text-xs font-semibold leading-none text-red-100 bg-red-400 rounded-full">
 							3
 						</span>
-					</div>
+					</div> */}
 					<hr className="w-0 h-7 border border-solid border-l border-gray-300 mx-3" />
-					<div className="flex items-center gap-3 cursor-pointer">
+					{/* <div className="flex items-center gap-3 cursor-pointer">
 						<UserIcon className="w-8 h-8" />
 						<span className="font-semibold">John Doe</span>
-					</div>
+					</div> */}
 				</div>
 				<Link href="/settings">
 					<SettingsIcon className="cursor-pointer text-gray-500" size={24} />
